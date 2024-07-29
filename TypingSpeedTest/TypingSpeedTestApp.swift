66159438cea10d046d7c430dc0f8058a85b5a69c
JiyanAtalay <jyntly23@gmail.com>
2024-07-29 21:54:21 +0300

    Initial Commit

---
+++ TypingSpeedTest/TypingSpeedTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypingSpeedTestApp.swift
+//  TypingSpeedTest
+//
+//  Created by Mehmet Jiyan Atalay on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TypingSpeedTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
